dc8d68291efea8cc04b12fc16fa8e0d12a0a5525
Erhard Siegl <erhard.siegl@gepardec.com>
2026-01-18 14:12:30 +0100

    Use Lib

--- vars/printEnv.groovy
+++ vars/printEnv.groovy
@@ -1,8 +1,23 @@
 #!/usr/bin/env groovy
 
-
+import hudson.model.*
+import hudson.util.TaskListener
+ 
 def call(String name = 'human') {
+
   echo "Env, ${name}."
   println(System.env)
+
+  def build = Thread.currentThread().executable
+  def envVars = build.getEnvironment(TaskListener.NULL)
+ 
+  // Access the BUILD_NUMBER variable
+  def buildNumber = envVars.get('BUILD_NUMBER')
+ 
+  if (buildNumber) {
+      println "Current build number: ${buildNumber}"
+  } else {
+      println "BUILD_NUMBER not found!"
+  }
 }
 
