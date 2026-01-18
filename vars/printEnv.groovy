90e8c7bda283d9da0da1777f52a5800a14aac951
Erhard Siegl <erhard.siegl@gepardec.com>
2026-01-18 14:47:11 +0100

    add HOME

--- vars/printEnv.groovy
+++ vars/printEnv.groovy
@@ -6,17 +6,7 @@ import hudson.model.TaskListener
 def call(String name = 'human') {
 
   echo "Env, ${name}."
-
-  def build = Thread.currentThread().executable
-  def envVars = build.getEnvironment(TaskListener.NULL)
- 
-  // Access the BUILD_NUMBER variable
-  def buildNumber = envVars.get('BUILD_NUMBER')
- 
-  if (buildNumber) {
-      println "Current build number: ${buildNumber}"
-  } else {
-      println "BUILD_NUMBER not found!"
-  }
+  def envVar = System.env['HOME']
+  echo "HOME: ${envVar}."
 }
 
